05bc42bc74d08e610963c48dafb4370c2d72cc1a
Chidiebere Uduh <udugreg@gmail.com>
2025-07-24 14:56:39 +0100

    practiced how to properly slice throgh tuple, list and dictionary

---
+++ slicing.py
@@ -0,0 +1,37 @@
+planet1="Closest to Sun"
+print(planet1)
+
+"""
+print(planet1[1])
+print(planet1[2])
+print(planet1[3])
+
+print(planet1[-1])
+print(planet1[-2])
+print(planet1[-3])
+
+
+#Slicing a string to get a substring from a string
+print(planet1[1:4])
+print(planet1[:])
+print(planet1[:9])
+
+#Slicing  Tuple
+devOps=("Linux","Vagrant","Bash-Scripting", "AWS", "Jenkins", "Python", "Ansible")
+print(devOps)
+
+
+print(devOps[1])
+print(devOps[2])
+
+print(devOps[0:3])
+# Slicing Tuple further
+print(devOps[0:3][1])
+print(devOps[0:3][1][3:7]"""
+
+# Dictionary example
+skills = {"devops": ("AWS", "jenkins", "Python", "Ansible"), "Developement": ["Java", "node Js", ",net"]}
+print(skills)
+print(skills["devops"])
+print(skills["devops"][1])
+print(skills["devops"][1][:3])
