0adbbd649c25d96ff7b0b78945a918128f9bddd5
Chidiebere Uduh <udugreg@gmail.com>
2025-07-24 13:19:57 +0100

    printing techniques

---
+++ printing.py
@@ -0,0 +1,16 @@
+name = "De nero"
+location = "Obigbo"
+
+print("The name of our Bar is", name)
+# Or
+print("The name of our Bar is", name, "at location", location)
+#another way of printing variables
+print("The our Bar is at {}".format(location))
+# Or
+print("Come to {} at {} and have a cold beer".format(name, location))
+# Or the f print function
+
+print(f"Come to {name} at {location} and have a cold beer")
+
+#Or with conacatenization
+print("The name of the Bar is" + " " + name)
