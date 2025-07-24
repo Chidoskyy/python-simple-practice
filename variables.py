1a3a682445ffef26d86f038e0c5bd1fe49ac995b
Chidiebere Uduh <udugreg@gmail.com>
2025-07-24 10:45:59 +0100

    List, turple and dictionary

---
+++ variables.py
@@ -0,0 +1,42 @@
+# Variable Assigments
+var1 = "pyton"
+var2 = 75
+var3 = 3.5
+#Printing Varaibles
+"""print(var1)
+print(var2)
+print(var3)
+"""
+a = b = c = 70
+print(a)
+print(b)
+print(c)
+
+x, y, z = "alpha", "beta", 12
+
+print("valuable x value is,", x)
+print("valuable y value is,", y)
+print("valuable z value is,", z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# List / this is a collection of multi datatypes, enclosed in square
+first_list = ["devops", 48, 4.6, True]
+
+# Printinting list
+print(first_list)
+
+# Turple / this is a collection of multi datatypes, enclosed in bracket
+first_turple = ("devops", 48, 4.6, True)
+
+# Printing Turple
+print(first_turple)
+
+# Dictionary / elements here are in key-value pairs and its enclosed in curly brackets
+first_dictionary = {"Name":"Chidi", "Age":89, "Track":"Devops", "Excersise":["Football", "Jogging"]}
+print(first_dictionary)
+print("variable first_list is a:", type(first_list))
+print("variable first_turple is a:", type(first_turple))
+print("variable first_dictionary is a:", type(first_dictionary))
